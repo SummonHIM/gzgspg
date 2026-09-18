8c5ffbc9c39f9db3ce9f1dcc3432eeedecd24c44
SummonHIM <summonhim@summonhim.top>
2026-09-19 00:09:45 +0800

    fix: 启动失败时以非零退出码退出

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/summonhim/gzgspg/internal/ui"
 )
 
 func main() {
 	if err := ui.Run(); err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
